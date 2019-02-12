b34ce04abf0d21a59b54b56872dd459ef05b99fc
star <653945222@qq.com>
2019-02-12 14:41:56 +0800

    day03-freemarker静态化测试通过模板文件和字符串

---
+++ test-freemarker/src/test/java/com/xuecheng/test/freemarker/FreemarkTest.java
@@ -0,0 +1,86 @@
+package com.xuecheng.test.freemarker;
+
+import freemarker.cache.StringTemplateLoader;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.assertj.core.util.Maps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class FreemarkTest {
+    //基于ftl文件生成html文件
+    @Test
+    public void generatorHtmlByFtl() throws IOException, TemplateException {
+        //创建配置类
+        Configuration configuration = new Configuration(Configuration.getVersion());
+        //设置模板路径
+        String path = this.getClass().getResource("/").getPath();
+        configuration.setDirectoryForTemplateLoading(new File(path + "/templates/"));
+        //设置字符集
+        configuration.setDefaultEncoding("utf-8");
+        //加载模块
+        Template template = configuration.getTemplate("test1.ftl");
+        //数据模块
+        Map map = new HashMap();
+        map.put("name", "star");
+        //静态化
+        String content = FreeMarkerTemplateUtils.processTemplateIntoString(template, map);
+        System.out.println(content);
+
+        //输出文件
+        FileUtils.writeByteArrayToFile(new File("/work/test1.html"), content.getBytes(StandardCharsets.UTF_8));
+
+    }
+
+    //基于ftl文件生成html文件
+    @Test
+    public void generatorHtmlByString() throws IOException, TemplateException {
+        //创建配置类
+        Configuration configuration = new Configuration(Configuration.getVersion());
+        //设置模板路径
+        //String path = this.getClass().getResource("/").getPath();
+        //configuration.setDirectoryForTemplateLoading(new File(path + "/templates/"));
+        ////设置字符集
+        //configuration.setDefaultEncoding("utf-8");
+        String templateString="" +
+                "<html>\n" +
+                "    <head></head>\n" +
+                " <body>\n" +
+                " 名称:${name}\n" +
+                "    </body>\n" +
+                "</html>";
+        //加载模块
+        StringTemplateLoader stringTemplateLoader = new StringTemplateLoader();
+        stringTemplateLoader.putTemplate("template", templateString);
+        configuration.setTemplateLoader(stringTemplateLoader);
+        //得到模板
+        Template template = configuration.getTemplate("template","UTF-8");
+
+        //数据模块
+        Map map = new HashMap();
+        map.put("name", "star");
+        //静态化
+        String content = FreeMarkerTemplateUtils.processTemplateIntoString(template, map);
+        System.out.println(content);
+
+        //输出文件
+        FileUtils.writeByteArrayToFile(new File("/work/test2.html"), content.getBytes(StandardCharsets.UTF_8));
+
+    }
+
+
+}
---
+++ test-freemarker/src/test/resources/templates/test1.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>hello freemarker</title>
+</head>
+<body>
+    hello ${name} !
+
+</body>
+</html>
